af121a72a626db08976938f29035ac029d68803d
LuisCruz1027 <luis.cruz04@utrgv.edu>
2024-05-17 18:01:16 -0500

    finished project screen

--- src/App.js
+++ src/App.js
@@ -5,27 +5,43 @@ import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
+import css from './cssBackground.png';
+import dungeonGen from './dungeonGen.png';
+import homebrew from './homebrew.png';
+import lungs from './lung_ct.jpg';
+import unity from './unity.png';
+
 const App = () => {
   const projectSites = [
     {
       name: "This Website!",
+      background: css,
       url: "https://github.com/Luis-Cruz1027/LuisCruzPortfolio",
-      description: "This project is a website to house all the links to my projects on Github, as well as describe me as a person. It uses the react framework to help create components that will display when specific routes are called. I used react because it is the framework I am most familiar with (for now) and because it is great for making web applications."
+      description: "This project is a website to house all the links to my projects on Github, as well as describe me as a person. It uses the react framework to help create components that will display when specific routes are called. I used react because it is the framework I am most familiar with (for now) and because it is great for making web applications. This is not the final look!"
     },
     {
       name: "Dungeons of the Multiverse",
+      background: dungeonGen,
       url: "https://github.com/Luis-Cruz1027/DungeonsOfTheMultiverse",
       description: "This project is a collaboration between Luis Cruz and Gilberto Gonzalez. The aim of this project is to create an application to be used as a tool for players of the tabletop role-playing game, Dungeons and Dragons. Whether the player is new to DND or not, whether they're in a party, it doesn't matter. The goal is to create a dynamic experience using OpenAI's API to allow the users to interact with chatGPT. This project is created in the Unity environment and makes use of it's premade tools."
     },
     {
       name: "Homebrew",
+      background: homebrew,
       url: "https://gitfront.io/r/LuisCruz1027/WN3CjXYLsfd4/homebrew-fixed-LuisCruz1027/",
       description: "This project utilizes Flutter to create a phone application that changes states as you navigate through the screens."
     },
     {
       name: "Digital Image Processing",
+      background: lungs,
       url: "https://github.com/LCRazo/digital-image-processing",
       description: "This project aims to perform various image processing tasks on a lung CT image. Tasks include loading and displaying the image, computing the 2D FFT, down-sampling the image, interpolating in both frequency and spatial domains, and computing errors between the original and interpolated images."
+    },
+    {
+      name: "Video Game (Title TBD)",
+      background: unity,
+      url: "https://github.com/Luis-Cruz1027/RatMonkeyProject1",
+      description: "This is a game currently in the works by Luis Cruz and Adam Perez. The aim is to create a fun user experience that changes through each playthrough. This game will be published through Steam once it reaches a state that is acceptable and will recieve updates until the game is in a finished state.",
     },
   ]
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,29 @@
 import PropTypes from 'prop-types';
 import './Projects.css';
 
-
-const Projects = ({sites}) => {
-    return(
+const Projects = ({ sites }) => {
+    return (
         <>
             <div className="site-list">
                 {sites.map((site, index) => (
                     <div key={index} className="site-item">
+                        <img class='background' src={site.background} />
                         <div className='site-info'>
-                            <h2>{site.name}</h2>
+                            <h2 className='title'>{site.name}</h2>
                             <button onClick={() => window.open(site.url, '_blank')}>View Repository</button>
-                            <h5>{site.description}</h5>
+                            <h5 className='site-description'>{site.description}</h5>
                         </div>
                     </div>
                 ))}
             </div>
+            <div className="more-on-the-way-container">
+                <div className="more-on-the-way-card">
+                    <p>More projects on the way!</p>
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
 
 Projects.propTypes = {
     sites: PropTypes.arrayOf(
@@ -29,4 +34,4 @@ Projects.propTypes = {
     ).isRequired
 };
 
-export default Projects
+export default Projects;
