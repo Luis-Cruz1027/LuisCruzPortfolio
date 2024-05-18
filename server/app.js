41aec1357d159df30462a23d7752b8d9c5ab0ba5
LuisCruz1027 <luis.cruz04@utrgv.edu>
2024-05-17 19:55:54 -0500

    server continued

--- server/app.js
+++ server/app.js
@@ -13,14 +13,14 @@ const io = new Server(server , {
 
 
 const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "../client/build");
+const buildPath = path.join(_dirname  , "../public");
 
 app.use(express.static(buildPath))
 
 app.get("/*", function(req, res){
 
     res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
+        path.join(__dirname, "../public/index.html"),
         function (err) {
           if (err) {
             res.status(500).send(err);
